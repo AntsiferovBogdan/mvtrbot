ca3908089afbf5f2a683f50af6377e779448bcef
Bogdan Antsiferov <7982777772l@gmail.com>
2020-01-02 12:44:58 +0500

    added email validation

--- handlers.py
+++ handlers.py
@@ -2,6 +2,8 @@ import logging
 
 from utils import *
 
+import re
+
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
                     filename='bot.log'
@@ -12,20 +14,24 @@ def greet_user(bot, update, user_data):
     text = 'Привет, {} {}'.format(update.message.chat.first_name, emoji)
     update.message.reply_text(text, reply_markup=get_keyboard())
 
+
 def registration_start(bot, update, user_data):
 	update.message.reply_text('Введите свой e-mail', reply_markup=ReplyKeyboardRemove())
 	return 'email'
 
+
 def registration_get_email(bot, update, user_data):
 	user_email = update.message.text
-	if '@' and '.' not in user_email:
-		update.message.reply_text('Проверьте корректность введенного e-mail')
-		return 'email'
-	else:
-		logging.info('User: %s, Chat ID: %s, E-Mail: %s', update.message.chat.username,
+	pattern = re.compile('^[\w\.]+[-\w]+@+([\w]([-\w]{0,61}[\w])\.)+[a-zA-Z]{2,6}$')
+	if re.match(pattern, user_email):
+		logging.info('user: %s, chat_id: %s, email: %s', update.message.chat.username,
         update.message.chat.id, update.message.text)
 		update.message.reply_text('Спасибо!')
+	else:
+		update.message.reply_text('Проверьте корректность введенного e-mail')
+		return 'email'
 	return ConversationHandler.END
+
 
 def dontknow(bot, update, user_data):
     update.message.reply_text('Проверьте корректность введенного e-mail')
