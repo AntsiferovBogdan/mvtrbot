636de077e8c1e7571b99e3c20cc5ff2bb4f31c2d
Bogdan Antsiferov <7982777772l@gmail.com>
2020-01-07 11:43:30 +0500

    db filling process

--- handlers.py
+++ handlers.py
@@ -1,5 +1,6 @@
 import logging
 
+from model import Users_tab, md
 from utils import *
 
 import re
@@ -9,6 +10,7 @@ logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     filename='bot.log'
                     )
 
+
 def greet_user(bot, update, user_data):
     emoji = get_user_emoji(user_data)
     text = 'Привет, {} {}'.format(update.message.chat.first_name, emoji)
@@ -16,23 +18,41 @@ def greet_user(bot, update, user_data):
 
 
 def registration_start(bot, update, user_data):
-	update.message.reply_text('Введите свой e-mail', reply_markup=ReplyKeyboardRemove())
-	return 'email'
+    update.message.reply_text(
+                              'Введите свой e-mail',
+                              reply_markup=ReplyKeyboardRemove()
+                              )
+    return 'email'
 
 
 def registration_get_email(bot, update, user_data):
-	user_email = update.message.text
-	pattern = re.compile('^[\w\.]+[-\w]+@+([\w]([-\w]{0,61}[\w])\.)+[a-zA-Z]{2,6}$')
-	if re.match(pattern, user_email):
-		logging.info('user: %s, chat_id: %s, email: %s', update.message.chat.username,
-        update.message.chat.id, update.message.text)
-		update.message.reply_text('Спасибо!')
-	else:
-		update.message.reply_text('Проверьте корректность введенного e-mail')
-		return 'email'
-	return ConversationHandler.END
+    user_email = update.message.text
+    pattern = re.compile(
+                         r'^[\w\.]+[-\w]+@+([\w]([-\w]{0,61}[\w])\.)+[a-zA-Z]{2,6}$'
+                         )
+    if re.match(pattern, user_email):
+        logging.info('user: %s, chat_id: %s, email: %s',
+                     update.message.chat.username,
+                     update.message.chat.id,
+                     update.message.text
+                     )
+        update.message.reply_text('Спасибо!')
+        add_user(update)
+    else:
+        update.message.reply_text('Проверьте корректность введенного e-mail')
+        return 'email'
+    return ConversationHandler.END
 
 
 def dontknow(bot, update, user_data):
     update.message.reply_text('Проверьте корректность введенного e-mail')
     return 'email'
+
+
+def add_user(update):
+    new_user = Users_tab(id=update.message.chat.id,
+                         user=update.message.chat.username,
+                         email=update.message.text
+                         )
+    md.session.add(new_user)
+    md.session.commit()
--- moviebot.py
+++ moviebot.py
@@ -1,9 +1,11 @@
 import logging
 
-from telegram.ext import (Updater, CommandHandler, 
-            MessageHandler, Filters, ConversationHandler)
+from telegram.ext import (Updater, CommandHandler,
+                          MessageHandler, Filters, ConversationHandler
+                          )
 
 from handlers import *
+
 import settings
 
 
@@ -17,23 +19,27 @@ def main():
     mybot = Updater(settings.API_KEY, request_kwargs=settings.PROXY)
 
     logging.info('Бот запускается')
-    
+
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user, pass_user_data=True))
-    
+
     registration = ConversationHandler(
-        entry_points=[(MessageHandler(Filters.regex('Зарегистрироваться'), registration_start, pass_user_data=True))],
+        entry_points=[(MessageHandler(Filters.regex('Зарегистрироваться'),
+                      registration_start, pass_user_data=True))
+                      ],
         states={
             'email': [MessageHandler(Filters.text, registration_get_email, pass_user_data=True)]
         },
-        fallbacks=[MessageHandler(Filters.photo | Filters.video | Filters.document, dontknow, pass_user_data=True)]
+        fallbacks=[MessageHandler(
+                                  Filters.photo | Filters.video | Filters.document,
+                                  dontknow, pass_user_data=True
+                                  )]
     )
     dp.add_handler(registration)
-
 
     mybot.start_polling()
     mybot.idle()
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
