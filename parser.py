e495b277426884fdf28852813cde419cb61e48d2
Bogdan Antsiferov <7982777772l@gmail.com>
2020-01-20 14:04:36 +0500

    parsing fix

--- parser.py
+++ parser.py
@@ -13,13 +13,34 @@ def get_html(url):
         return False
 
 
-def get_price(html):
-    if html:
-        soup = BeautifulSoup(html, 'html.parser')
-        for a in soup.find_all('a', href=True):
-            print(a)
+def get_url_ivi(html_ivi):
+    if html_ivi:
+        soup = BeautifulSoup(html_ivi, 'html.parser')
+        search = soup.find('li', class_='gallery__item').find('a')
+        list_ = []
+        if search:
+            list_.append(search['href'])
+        print(f'Смотрите фильм {user_input.capitalize()} в онлайн-кинотеатре ivi: ivi.ru{list_[0]}')
     return False
 
 
-html = get_html('https://okko.tv/search/' + input())
-get_price(html)
+def get_url_megogo(html_megogo):
+    if html_megogo:
+        soup = BeautifulSoup(html_megogo, 'html.parser')
+        search = soup.find('div', class_='card-content video-content').find('a')
+        list_ = []
+        if search:
+            list_.append(search['href'])
+        print(f'Смотрите фильм {user_input.capitalize()} в онлайн-кинотеатре megogo: megogo.ru{list_[0]}')
+    return False
+
+
+user_input = input()
+user_input_del_spaces = user_input.split()
+user_input_no_spaces = '-'.join(user_input_del_spaces)
+
+html_ivi = get_html('https://www.ivi.ru/search/?q=' + user_input_no_spaces)
+html_megogo = get_html('https://megogo.ru/ru/search-extended?q=' + user_input_no_spaces)
+
+get_url_ivi(html_ivi)
+get_url_megogo(html_megogo)
