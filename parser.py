85bd882dd4ade93f9e6d5fd9978640b6719b03dc
Bogdan Antsiferov <7982777772l@gmail.com>
2020-01-31 15:39:31 +0500

    heroku fix

--- parser.py
+++ parser.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import re
 import requests
 import settings
@@ -10,14 +11,6 @@ from utils import get_confirm_keyboard, get_keyboard
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from telegram import ReplyKeyboardRemove
-
-GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
-CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
-
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.binary_location = GOOGLE_CHROME_PATH
 
 
 def searching_start(bot, update):
@@ -76,8 +69,12 @@ def get_url_ivi(bot, update):
         'Ищу цены, подождите, пожалуйста...',
         reply_markup=ReplyKeyboardRemove()
         )
-    driver = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH,
-                              chrome_options=chrome_options)
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     driver.get('http://' + url_ivi)
     logging.info
     #try:
