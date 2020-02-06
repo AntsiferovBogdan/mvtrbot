07b4022f3c6386f7a29615eef160c5e5b894e507
Bogdan Antsiferov <7982777772l@gmail.com>
2020-02-06 17:44:56 +0500

    ivi error

--- moviebot.py
+++ moviebot.py
@@ -4,8 +4,8 @@ from telegram.ext import (Updater, CommandHandler,
                           MessageHandler, Filters, ConversationHandler
                           )
 
-from handlers import (greet_user, registration_start,
-                      registration_get_email, dontknow
+from handlers import (check_email, greet_user,
+                      registration_start, registration_get_email
                       )
 
 from parser import (incorrect_movie, get_url_ivi, get_url_megogo,
@@ -25,28 +25,39 @@ def main():
     logging.info('Бот запускается')
 
     dp = mybot.dispatcher
-    dp.add_handler(CommandHandler('start', greet_user, pass_user_data=True))
+    dp.add_handler(CommandHandler('start', greet_user, pass_user_data=True,
+                                  pass_chat_data=True)
+                   )
 
     registration = ConversationHandler(
         entry_points=[(MessageHandler(Filters.regex('Зарегистрироваться'),
-                      registration_start)),
+                      registration_start, pass_user_data=True,
+                      pass_chat_data=True)
+                       ),
                       (MessageHandler(Filters.regex('Найти фильм'),
-                       searching_start))
+                       searching_start, pass_user_data=True,
+                       pass_chat_data=True)
+                       )
                       ],
         states={
-            'email': [MessageHandler(Filters.text, registration_get_email)
+            'email': [MessageHandler(Filters.text, registration_get_email,
+                                     pass_user_data=True, pass_chat_data=True)
                       ],
-            'search_movie': [MessageHandler(Filters.text, search_movie)
+            'search_movie': [MessageHandler(Filters.text, search_movie,
+                                            pass_user_data=True,
+                                            pass_chat_data=True)
                              ],
             'confirm': [(MessageHandler(Filters.regex('Да'),
-                        get_url_megogo)),
+                        get_url_ivi, pass_user_data=True,
+                        pass_chat_data=True)),
                         (MessageHandler(Filters.regex('Нет'),
-                         incorrect_movie))
+                         incorrect_movie, pass_user_data=True,
+                         pass_chat_data=True))
                         ]
         },
         fallbacks=[MessageHandler(
                                   Filters.photo | Filters.video |
-                                  Filters.document, dontknow
+                                  Filters.document, check_email
                                   )]
     )
     dp.add_handler(registration)
