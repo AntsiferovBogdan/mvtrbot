81027f2b8a470a27046b6adff48c7b54821d454d
Диана <diana@MacBook-Pro-Diana.local>
2020-02-12 21:10:02 +0300

    ivi parser works

--- parser.py
+++ parser.py
@@ -64,8 +64,9 @@ def search_movie(bot, update, user_data, chat_data):
         search_director_kp = soup.find_all('i', class_='director')
         global director_kp
         director_kp = (''.join(re.findall(
-            r'[а-я А-Я]', search_director_kp[i].text))
-            ).split('реж ')
+            r'[а-яА-Я]', search_director_kp[i].text))
+            ).split('реж')
+        print(director_kp)
         update.message.reply_text(
             f'Вас интересует данный фильм? {title_kp}, {year_kp} {poster_kp}',
             reply_markup=get_confirm_keyboard()
@@ -74,24 +75,24 @@ def search_movie(bot, update, user_data, chat_data):
 
 
 def incorrect_movie(bot, update, user_data, chat_data):
-    #global i
+    global i
     i += 1
     search_title_kp = search_info_kp[i].find('a')
-    #global title_kp
+    global title_kp
     title_kp = ''.join(re.findall(r'[а-я А-Я0-9I]', search_title_kp.text))
     search_year_kp = search_info_kp[i].find('span')
-    #global year_kp
+    global year_kp
     year_kp = ''.join(re.findall(r'[0-9]', search_year_kp.text))
     search_poster_kp = soup.find_all(class_='pic')
     search_poster_kp = search_poster_kp[i].find(class_="js-serp-metrika").get(
         'data-id'
         )
-    #global poster_kp
+    global poster_kp
     poster_kp = f'https://st.kp.yandex.net/images/film_iphone/iphone360_{search_poster_kp}.jpg'
-    #global director_kp
+    global director_kp
     director_kp = (''.join(re.findall(
-        r'[а-я А-Я]', search_director_kp[i].text))
-        ).split('реж ')
+        r'[а-яА-Я]', search_director_kp[i].text))
+        ).split('реж')
     update.message.reply_text(
         f'Вас интересует данный фильм? {title_kp}, {year_kp} {poster_kp}',
         reply_markup=get_confirm_keyboard()
@@ -121,12 +122,12 @@ def get_url_megogo(bot, update, user_data, chat_data):
         director_megogo = ''.join(re.findall(
             r'[а-я А-Я]', director_search_megogo.text)
             )
-
+        print(director_megogo)
         if title_kp == title_megogo and director_kp[1] == director_megogo:
-            get_price_megogo(bot, update)
+            return get_price_megogo(bot, update, user_data, chat_data)
         else:
             m += 1
-            get_url_megogo(bot, update)
+            return get_url_megogo(bot, update, user_data, chat_data)
     return False
 
 
@@ -134,36 +135,46 @@ def get_url_ivi(bot, update, user_data, chat_data):
     title_fix = '+'.join(title_kp.split())
     url = f'http://www.ivi.ru/search/?q={title_fix}'
     html_ivi = get_html(url)
+
     if html_ivi:
         soup_ivi = BeautifulSoup(html_ivi, 'html.parser')
-
-        search_title_ivi = soup_ivi.find_all(class_='nbl-slimPosterBlock__title')
+        search_title_ivi = soup_ivi.find_all(
+            class_='nbl-slimPosterBlock__title'
+            )
         global iv
         title_ivi = ''.join(re.findall(
             r'[а-я А-Я]', search_title_ivi[iv].text)
             )
         global url_ivi
         search_url_ivi = soup_ivi.find_all(class_='gallery__item')
-        url_ivi = 'ivi.ru' + search_url_ivi[iv].find('a').get('href')
-        url = url_ivi
-        print(url)
-        html_director = get_html(url)
-        soup_ivi_director = BeautifulSoup(html_director, 'html.parser')
-        director_search_ivi = soup_ivi_director.find_all(class_='gallery__item')
-        print(director_search_ivi)
-        director_ivi = ''.join(re.findall(
-            r'[а-я А-Я]', director_search_ivi.text)
+        url_ivi = 'http://ivi.ru' + search_url_ivi[iv].find('a').get('href')
+        director_html_ivi = get_html(url_ivi)
+        director_parsing_ivi = BeautifulSoup(director_html_ivi, 'html.parser')
+        director_search_ivi = director_parsing_ivi.find(
+            class_='gallery__item'
             )
-        print(director_ivi)
-        #if title_kp == title_ivi and director_kp[1] == director_ivi:
-            #get_price_ivi(bot, update)
-        #else:
-            #iv += 1
-            #get_all_urls(bot, update)
+        director_ivi = (''.join(re.findall(
+            r'[а-я А-Я]', director_search_ivi.text))
+            ).split('режисср')
+        print(title_kp)
+        print(title_ivi)
+        print(director_kp[1])
+        print(director_ivi[0])
+
+        if title_kp == title_ivi and director_kp[1] == director_ivi[0]:
+            return get_price_ivi(bot, update, user_data, chat_data)
+        else:
+            iv += 1
+            return get_url_ivi(bot, update, user_data, chat_data)
     return False
 
 
 def get_price_megogo(bot, update, user_data, chat_data):
+    update.message.reply_text(
+        'Ищу цены, подождите, пожалуйста...',
+        reply_markup=ReplyKeyboardRemove()
+        )
+
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
@@ -174,13 +185,13 @@ def get_price_megogo(bot, update, user_data, chat_data):
     time.sleep(3)
     element_1 = driver.find_element_by_class_name('play-icon')
     element_1.click()
-    time.sleep(2)
+    time.sleep(5)
     price_page = driver.page_source
     soup_megogo_price = BeautifulSoup(price_page, 'html.parser')
     find_subscribe = soup_megogo_price.find('p', class_='stub-description')
     if 'доступен' in find_subscribe.text:
         update.message.reply_text(
-            f'Фильм доступен по подписке'  # через re вытянуть полный текст с мегого!
+            f'Фильм доступен по подписке'  # вытянуть полный текст с мегого!
             )
     else:
         search_price = soup.find_all(class_='pQualityItemPrice__value')
@@ -193,20 +204,20 @@ def get_price_megogo(bot, update, user_data, chat_data):
             )
 
 
-def get_url_ivi_2(bot, update):  # под удаление
+def get_price_ivi(bot, update, user_data, chat_data):
     update.message.reply_text(
         'Ищу цены, подождите, пожалуйста...',
         reply_markup=ReplyKeyboardRemove()
         )
+
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(
-        executable_path=settings.CHROME_DRIVER_URL, options=chrome_options
-        )
-    driver.get('http://' + url_ivi)
-    time.sleep(5)
-    logging.info
+        executable_path=settings.CHROME_DRIVER_URL)
+    # options=chrome_options
+    driver.get(url_ivi)
+    time.sleep(3)
 
     try:
         element_1 = driver.find_element_by_id('js-erotic-confirm')
@@ -222,10 +233,10 @@ def get_url_ivi_2(bot, update):  # под удаление
         price_page = driver.page_source
         soup = BeautifulSoup(price_page, 'html.parser')
         search_bell = soup.find_all(class_='nbl-button__primaryText')
-        search_bell_in = ''.join((re.findall(r'[а-я А-Я]', search_bell[1].text)))
-        if 'Сообщить о появлении' in search_bell_in:
+        bell = ''.join((re.findall(r'[а-я А-Я]', search_bell[1].text)))
+        if 'Сообщить о появлении' in bell:
             update.message.reply_text(
-                'К сожалению, данный фильм отсутствует в онлайн-кинотеатре на текущий момент.',
+                'К сожалению, данный фильм отсутствует в онлайн-кинотеатре.',
                 reply_markup=get_keyboard()
                 )
             return ConversationHandler.END
@@ -243,10 +254,13 @@ def get_url_ivi_2(bot, update):  # под удаление
             subscribe_in = ''.join((re.findall(r'[а-яА-Я]', subscribe.text)))
             if 'Подписка' in subscribe_in:
                 update.message.reply_text(
-                    f"Смотрите фильм '{info}' в онлайн-кинотеатре ivi: {url_ivi}"
+                    f"Смотрите '{title_kp}' в онлайн-кинотеатре ivi: {url_ivi}"
                     )
                 update.message.reply_text(
-                    'Данный фильм доступен по подписке ivi. Также Вы можете приобрести его в HD/SD качестве за 399/299₽.',
+                    'Данный фильм доступен по подписке ivi.',
+                    )
+                update.message.reply_text(
+                    'Также Вы можете купить его в HD/SD качестве за 399/299₽.',
                     reply_markup=get_keyboard()
                     )
                 return ConversationHandler.END
@@ -256,7 +270,7 @@ def get_url_ivi_2(bot, update):  # под удаление
                 soup = BeautifulSoup(price_page, 'html.parser')
                 search_prices = soup.find_all(class_='plateTile__caption')
                 update.message.reply_text(
-                                f"Смотрите фильм '{info}' в онлайн-кинотеатре ivi: {url_ivi}"
+                                f"Смотрите фильм '{title_kp}' в онлайн-кинотеатре ivi: {url_ivi}"
                                 )
                 price_buy_hd = re.findall(r'\d', search_prices[0].text)
                 update.message.reply_text(
