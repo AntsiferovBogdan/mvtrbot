1756fea6b7624cf46e77378f233a3e93e182fd7c
Диана <diana@MacBook-Pro-Diana.local>
2020-02-15 17:02:55 +0300

    settings

---
+++ settings.py
@@ -0,0 +1,21 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+PROXY = {'proxy_url': 'socks5://t2.learn.python.ru:1080',
+         'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}
+         }
+
+API_KEY = ('907137899:AAE3njkeOX3agXvToU-G3vIXMGPFdTaZ4NY')
+
+USER_EMOJI = [':smiley_cat:', ':smiling_imp:', ':panda_face:', ':dog:',
+              ':boy:', ':girl:', ':mouse:', ':frog:',
+              ':tiger:', ':new_moon_with_face:', ':crown:', ':alien:'
+              ]
+
+SQLALCHEMY_DATABASE_URL = 'sqlite:///' + os.path.join(basedir, 'users_db.db')
+
+CHROME_DRIVER_URL = '/Users/diana/projects/mvtrbot/chromedriver'
+
+OKKO_USERNAME = 'mvtrbot@mail.ru'
+OKKO_PASSWORD = 'testtest'
